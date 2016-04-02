0299e2d79860022fe8f5c8e5d40cc6e3c784a629
ahbing <givemearainbowlife@gmail.com>
2016-04-02 23:53:27 +0800

    add code style && ci

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # once
-just for fun
+![](https://travis-ci.org/ahbing/once.svg)
+So if you have something to say, [say it to me now](http://music.163.com/#/song?id=4340796).
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+var gulp = require('gulp');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+
+var paths = {
+  scripts: ['**/*.js', '!coverage/**/*.js', '!node_modules/**/*.js', '!public/**/*.js']
+};
+gulp.task('jshint', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jscs())
+    .pipe(jscs.reporter());
+});
+
+gulp.task('lint', ['jshint', 'jscs']);
+
